2ead1b449060dea98c6dcfe342b95021d4b932fc
Gutierrez <Alfonso.Gutierrez@chubb.com>
2022-01-02 23:32:29 -0600

    refactoring de modulos

--- src/test/java/starter/searchTestSuite.java
+++ src/test/java/starter/searchTestSuite.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(glue = {"step_definitions"},
-        features = "src/test/resources/features/searcher.feature")
+        features = "src/test/resources/features/ExecuteOrder.feature")
 
 public class searchTestSuite {
 }
--- src/test/java/step_definitions/SelectProducts.java
+++ src/test/java/step_definitions/SelectProducts.java
@@ -8,8 +8,6 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.devtools.Message;
-
 import java.util.concurrent.TimeUnit;
 
 public class SelectProducts {
@@ -32,12 +30,12 @@ public class SelectProducts {
     } @When("Seleccionar productos y agregarlos al carrito")
     public void seleccionar_productos() {
 
-        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
         //Selecciona el Galaxy S6, lo adiciona al carro y se regresa al menú principal
         driver.findElement(By.linkText("Samsung galaxy s6")).click();
         driver.findElement(By.partialLinkText("Add to cart")).click();
         driver.findElement(By.id("nava")).click();
-        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
 
         //Selecciona el Galaxy S7, lo adiciona al carro y se regresa al menú principal
         driver.findElement(By.linkText("Samsung galaxy s7")).click();
@@ -51,16 +49,15 @@ public class SelectProducts {
     }
 
       @Then("Confirmacion Productos Adicionados")
-    public void confirmacion_productos_adicionados() {
-       //Revisa el carro
+    public void confirmacion_productos_adicionados() throws InterruptedException {
+          //Revisa el carro
           driver.findElement(By.partialLinkText("Cart")).click();
-
-          //driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
-          String Total = String.valueOf(driver.findElement(By.xpath("//*[@id='totalp']")).getText());
-          //String Total = driver.findElement(By.xpath("//*[@id='totalp']")).getText();
+          Thread.sleep(1000);
+          driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
+          int TotalConfirmados = Integer.parseInt(driver.findElement(By.xpath("//*[@id=\"totalp\"]")).getText());
+          // Si el total en el carrito es menos de 1,950 significa que se agregaron todos los productos.
           // s6 360 s7 800, Vaio 790
-          //Assert.assertEquals(Total , "1590");
-          Assert.assertNotNull(Total);
+          Assert.assertEquals(TotalConfirmados , 1950);
     }
 //    @After
 //    public void tearDown(){
